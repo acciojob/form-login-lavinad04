180d0d9fbe4b4a96b531e6d1ba65e8d4559fd631
Lavina <150042947+lavinad04@users.noreply.github.com>
2024-02-26 19:42:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getFormvalue() {
     //Write your code here
 let inps = document.querySelector("input");
-let fName = inps[0];
-let lName = inps[1];
+let fname = inps[0].value;
+let lname = inps[1].value;
 let fullname = fName+lName;
 	alert(fullname); 
 }
